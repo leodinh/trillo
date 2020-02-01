4b712b3348637fd012fc946e3746aa4f08753b33
leodinh <NguyenAnhTuan.Dinh@georgebrown.ca>
2020-02-01 00:16:28 -0500

    Updates

---
+++ precache-manifest.eb36b7834467cd22862241337e4e36c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504da54076281cc184fab5e7fa5fa1c9",
+    "url": "/trillo/index.html"
+  },
+  {
+    "revision": "343b21481237df0ce791",
+    "url": "/trillo/static/css/main.07890c39.chunk.css"
+  },
+  {
+    "revision": "e5aeb194ebd949cbe37c",
+    "url": "/trillo/static/js/2.91eb9347.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/trillo/static/js/2.91eb9347.chunk.js.LICENSE"
+  },
+  {
+    "revision": "343b21481237df0ce791",
+    "url": "/trillo/static/js/main.921ee301.chunk.js"
+  },
+  {
+    "revision": "06fb0e3bd283a26b8bd6",
+    "url": "/trillo/static/js/runtime-main.b9909349.js"
+  },
+  {
+    "revision": "c7fedfa061fe1cdacbbfc58ca273898f",
+    "url": "/trillo/static/media/chevron-thin-right.c7fedfa0.svg"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trillo/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trillo/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trillo/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "ec019d84706f0348aeffc9f76a66f510",
+    "url": "/trillo/static/media/sprite.ec019d84.svg"
+  },
+  {
+    "revision": "a2a26d7906c257d584688e4a0130fe8c",
+    "url": "/trillo/static/media/user.a2a26d79.jpg"
+  }
+]);
